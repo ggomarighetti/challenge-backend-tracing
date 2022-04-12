8c9cecd1e297c9c8859638399cef5a6362c74f6c
Guillermo Orue Marighetti <59489394+ggomarighetti@users.noreply.github.com>
2022-04-12 16:52:48 -0300

    Created country service

---
+++ src/main/java/com/ggomarighetti/tracing/service/CountryService.java
@@ -0,0 +1,4 @@
+package com.ggomarighetti.tracing.service;
+
+public interface CountryService {
+}
---
+++ src/main/java/com/ggomarighetti/tracing/service/impl/CountryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ggomarighetti.tracing.service.impl;
+
+import com.ggomarighetti.tracing.service.CountryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CountryServiceImpl implements CountryService {
+}
