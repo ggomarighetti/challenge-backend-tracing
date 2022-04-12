19e4a68f82035c17f18e3678c9e3ef0089595fb8
Guillermo Orue Marighetti <59489394+ggomarighetti@users.noreply.github.com>
2022-04-12 16:52:57 -0300

    Created petition service

---
+++ src/main/java/com/ggomarighetti/tracing/service/PetitionService.java
@@ -0,0 +1,4 @@
+package com.ggomarighetti.tracing.service;
+
+public interface PetitionService {
+}
---
+++ src/main/java/com/ggomarighetti/tracing/service/impl/PetitionServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ggomarighetti.tracing.service.impl;
+
+import com.ggomarighetti.tracing.service.PetitionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetitionServiceImpl implements PetitionService {
+}
