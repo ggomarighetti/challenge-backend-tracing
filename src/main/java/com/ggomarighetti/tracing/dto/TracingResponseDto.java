99e31a34a128c69e7e8cd41236a95a67c22e8b48
Guillermo Orue Marighetti <59489394+ggomarighetti@users.noreply.github.com>
2022-04-13 17:01:12 -0300

    Created tracing response dto

---
+++ src/main/java/com/ggomarighetti/tracing/dto/TracingResponseDto.java
@@ -0,0 +1,21 @@
+package com.ggomarighetti.tracing.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Map;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TracingResponseDto {
+    private String countryName;
+    private String countryCode;
+    private Map<String, String> timezones;
+    private Double distance;
+    private Map<String, BigDecimal> currencies;
+}
