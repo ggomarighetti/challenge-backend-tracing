87f9a542d02ecc3f0831e29c76195d9a41e37645
Guillermo Orue Marighetti <59489394+ggomarighetti@users.noreply.github.com>
2022-04-13 16:59:47 -0300

    Updated country repository

--- src/main/java/com/ggomarighetti/tracing/repository/CountryRepository.java
+++ src/main/java/com/ggomarighetti/tracing/repository/CountryRepository.java
@@ -3,5 +3,9 @@ package com.ggomarighetti.tracing.repository;
 import com.ggomarighetti.tracing.entity.CountryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CountryRepository extends JpaRepository<CountryEntity, String> {
+
+    Optional<CountryEntity> findByName(String name);
 }
