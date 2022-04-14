81df6932916135b8ab936343a8082d9a5df6eeff
Guillermo Orue Marighetti <59489394+ggomarighetti@users.noreply.github.com>
2022-04-14 17:19:00 -0300

    Updated stats controller

--- src/main/java/com/ggomarighetti/tracing/controller/StatsController.java
+++ src/main/java/com/ggomarighetti/tracing/controller/StatsController.java
@@ -1,4 +1,22 @@
 package com.ggomarighetti.tracing.controller;
 
+import com.ggomarighetti.tracing.dto.StatsResponseDto;
+import com.ggomarighetti.tracing.service.StatsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stats")
 public class StatsController {
+
+    @Autowired
+    private StatsService statsService;
+
+    @GetMapping
+    public StatsResponseDto getStats() {
+        return statsService.getStats();
+    }
+
 }
