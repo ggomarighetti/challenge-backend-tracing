45f8f3c55512632f89cc0045c065a49d7f2eefef
Guillermo Orue Marighetti <59489394+ggomarighetti@users.noreply.github.com>
2022-04-14 17:19:11 -0300

    Updated stats response dto

--- src/main/java/com/ggomarighetti/tracing/dto/StatsResponseDto.java
+++ src/main/java/com/ggomarighetti/tracing/dto/StatsResponseDto.java
@@ -1,4 +1,14 @@
 package com.ggomarighetti.tracing.dto;
 
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class StatsResponseDto {
+    private double farthestDistance;
+    private double nearestDistance;
+    private double averageDistance;
 }
